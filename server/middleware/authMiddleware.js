c0a9b305dc6999877b1d836f8aa5176db611a106
Gaurav-Sng <mysteryofpride@gmail.com>
2025-10-19 16:35:34 +0530

    improved route

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const users = require('../models/userModel');
 const vendors = require('../models/vendorModel');
 const authHandler = async (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1];
+    const token = req.headers.authorization;
     if (!token) return res.status(401).json({ message: 'no token provided' });
 
     try {
